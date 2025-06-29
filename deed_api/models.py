4cabccae5ad700c96a11dc543a6f54b1fa840fea
nethaji7410 <ilangonethaji1@gmail.com>
2025-06-29 19:51:48 +0530

    update code comments

--- deed_api/models.py
+++ deed_api/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class ExcelData(models.Model):
-    tag_name = models.CharField(max_length=100)
-    document_name = models.CharField(max_length=100)
+    tag_name = models.CharField(max_length=100) #Stores a tag/category name for the document
+    document_name = models.CharField(max_length=100) # Stores the name of the document
     data = models.JSONField()  # Store row data
     created_at = models.DateTimeField(auto_now_add=True)
 
 class FieldPosition(models.Model):
-    field_name = models.CharField(max_length=100)
+    field_name = models.CharField(max_length=100) # # Stores the name of the field
     x = models.FloatField()
     y = models.FloatField()
-    is_global = models.BooleanField(default=False)
+    is_global = models.BooleanField(default=False) #If True, this position applies globally ,Use only for a specific form/template
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- deed_api/views.py
+++ deed_api/views.py
@@ -5,27 +5,31 @@ from .models import ExcelData
 from rest_framework import status
 from .models import FieldPosition
 from .serializers import FieldPositionSerializer
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import  IsAuthenticated
 
 # Create your views here.
 
+# Get distinct tag names from ExcelData table
 class TagListView(APIView):
 
-    permission_classes = [AllowAny]
+    # permission_classes = [AllowAny]
 
     def get(self, request):
         tags = ExcelData.objects.values_list('tag_name', flat=True).distinct()
         return Response({'tags': list(tags)})
 
 
+# Get unique document names under a specific tag
 class DocumentNameListView(APIView):
+    
 
-    permission_classes = [AllowAny] 
+    # permission_classes = [AllowAny] 
 
     def get(self, request, tag_name):
         documents = ExcelData.objects.filter(tag_name=tag_name)\
                     .values_list('document_name', flat=True).distinct()
         
+        # Return error if no documents found
         if not documents:
             return Response({'message': 'No documents found for this tag.'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -34,35 +38,40 @@ class DocumentNameListView(APIView):
 
 class SavePositionView(APIView):
 
-    permission_classes = [AllowAny]
+    # permission_classes = [AllowAny]
 
     def post(self, request):
         data = request.data
 
-        
+        # If only one object is sent, convert to list
         if isinstance(data, dict):
             data = [data]
 
+        # Validate and save using serializer
         serializer = FieldPositionSerializer(data=data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "Field positions saved."}, status=status.HTTP_201_CREATED)
+         # Return validation errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ResetPositionView(APIView):
-    permission_classes = [AllowAny]
+    # permission_classes = [AllowAny]
 
     def post(self, request):
+        # Delete all saved field positions
         FieldPosition.objects.all().delete()
         return Response({"message": "All field positions have been reset to default."}, status=status.HTTP_200_OK)
     
-    
+
 class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated] # Only logged-in users can logout
 
     def post(self, request):
         try:
+            # Delete the current user's token to logout
             request.user.auth_token.delete()
             return Response({'message': 'Successfully logout'}, status=status.HTTP_200_OK)
         except Exception as e:
+            # Return error if something fails
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
